c87edae712af95693889becc49fc56f86c690a40
cromartie1020 <hrc245@mail.com>
2024-11-27 00:42:46 -0500

    stil not working

--- calculator.py
+++ calculator.py
@@ -2,14 +2,30 @@ from tkinter import *
 # To load tkinter 'sudo apt install 
 root = Tk()
 root.title('Simple Calculator')
-e=  Entry(root, width=35 ,borderwidth=5)
+e=  Entry(root,width=35 ,borderwidth=5)
 e.grid(row=0,column=0,columnspan=3, padx=10,pady=10)
 
 def clickAdd(number):
-    e.insert(0,number)
+    first_number=e.get()
+    print('first_number',first_number)
+    global f_num  
+    f_num = first_number
+    print('f_num',f_num)
+    e.delete(0,END)
+    e.insert(f_num)
+    
     number=e.get()
     print(number)
     return
+def button_add():
+    return
+
+def button_clear():
+    return
+
+def button_equal():
+    retur        
+
 button_7 = Button(root, text='7', padx=40,pady=20 ,command=lambda:clickAdd(7)).grid(row=1,column=0)
 button_8 = Button(root, text='8', padx=40,pady=20,command=lambda:clickAdd(8) ).grid(row=1,column=1)
 button_9 = Button(root, text='9', padx=40,pady=20,command=lambda:clickAdd(9) ).grid(row=1,column=2)
@@ -19,6 +35,10 @@ button_6 = Button(root, text='6', padx=40,pady=20,command=lambda:clickAdd(6) ).g
 button_1 = Button(root, text='1', padx=40,pady=20,command=lambda:clickAdd(1) ).grid(row=3,column=0)
 button_2 = Button(root, text='2', padx=40,pady=20,command=lambda:clickAdd(2) ).grid(row=3,column=1)
 button_3 = Button(root, text='3', padx=40,pady=20,command=lambda:clickAdd(3) ).grid(row=3,column=2)
+button_0 = Button(root, text='0', padx=40,pady=20,command=lambda:clickAdd(0) ).grid(row=4,column=0)
+button_add = Button(root, text='+', padx=39,pady=20,command=button_add).grid(row=4,column=0)
+button_clear = Button(root, text='clear', padx=91,pady=20,command=button_clear ).grid(row=4,column=1)
+button_equal = Button(root, text='=',padx=40,pady=20,command=button_equal).grid(row=5,column=0)
 
 
 mainloop()
